a8ca73e73611157df9766f2f4a7977b0ecd8609d
lefajmofokeng <lefamjack@gmail.com>
2026-01-12 10:16:58 +0200

    Updated

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+
+// Prevents the right-click context menu from appearing anywhere on the page
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// You can add other global, shared scripts here as well
